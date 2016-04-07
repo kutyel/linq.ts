96b61a7ce0f5b52ff7c0f9be67d283de7f892efe
kutyel <flaviocorpa@gmail.com>
2016-04-07 14:45:09 +0200

    remove generators

--- linq.ts
+++ linq.ts
@@ -413,14 +413,14 @@ export class Enumerable {
     /**
      * Generates a sequence of integral numbers within a specified range.
      */
-    public static *Range(start: number, count: number): IterableIterator<number> {
-        while (count--) { yield start++; }
+    public static Range(start: number, count: number): List<number> {
+        let result = new List<number>(); while (count--) { result.Add(start++); } return result;
     }
 
     /**
      * Generates a sequence that contains one repeated value.
      */
-    public static *Repeat(element: any, count: number): IterableIterator<any> {
-        while (count--) { yield element; }
+    public static Repeat<T>(element: T, count: number): List<T> {
+        let result = new List<T>(); while (count--) { result.Add(element); } return result;
     }
 }
--- test/enum.ts
+++ test/enum.ts
@@ -2,17 +2,12 @@ import test from "ava";
 import {Enumerable} from "../linq";
 
 test("Range", t => {
-    let test = Enumerable.Range(20, 5);
-    t.is(test.next().value, 20);
-    t.is(test.next().value, 21);
-    t.is(test.next().value, 22);
-    t.is(test.next().value, 23);
-    t.is(test.next().value, 24);
+    t.is(Enumerable.Range(1, 10).Select(x => x * x).ToArray().toString(), "1,4,9,16,25,36,49,64,81,100");
 });
 
 test("Repeat", t => {
-    let test = Enumerable.Repeat("I like programming.", 3);
-    t.is(test.next().value, "I like programming.");
-    t.is(test.next().value, "I like programming.");
-    t.is(test.next().value, "I like programming.");
+    const test = Enumerable.Repeat("I like programming.", 3);
+    t.is(test.ElementAt(0), "I like programming.");
+    t.is(test.ElementAt(1), "I like programming.");
+    t.is(test.ElementAt(2), "I like programming.");
 });
