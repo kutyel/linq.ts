737cd13ad56de42d34975608204d169ccaa1f969
kutyel <flaviocorpa@gmail.com>
2016-05-04 21:27:12 +0200

    remove double quotes from enum tests

--- test/enum.ts
+++ test/enum.ts
@@ -1,13 +1,13 @@
-import test from "ava";
-import {Enumerable} from "../linq";
+import test from 'ava';
+import {Enumerable} from '../linq';
 
-test("Range", t => {
-    t.is(Enumerable.Range(1, 10).Select(x => x * x).ToArray().toString(), "1,4,9,16,25,36,49,64,81,100");
+test('Range', t => {
+    t.is(Enumerable.Range(1, 10).Select(x => x * x).ToArray().toString(), '1,4,9,16,25,36,49,64,81,100');
 });
 
-test("Repeat", t => {
-    const test = Enumerable.Repeat("I like programming.", 3);
-    t.is(test.ElementAt(0), "I like programming.");
-    t.is(test.ElementAt(1), "I like programming.");
-    t.is(test.ElementAt(2), "I like programming.");
+test('Repeat', t => {
+    const test = Enumerable.Repeat('I like programming.', 3);
+    t.is(test.ElementAt(0), 'I like programming.');
+    t.is(test.ElementAt(1), 'I like programming.');
+    t.is(test.ElementAt(2), 'I like programming.');
 });
