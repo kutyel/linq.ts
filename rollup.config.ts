843d622bd385740d906d4e06bcaf28ddda32e7ee
Asier Ferro <asierferro@gmail.com>
2017-10-11 10:43:19 +0200

    Removed comments on rollup config files

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,20 +12,13 @@ export default {
     { dest: pkg['umd:main'], moduleName: moduleName, format: 'umd' },
   ],
   sourcemap: true,
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
   watch: {
     include: 'dist/**',
   },
   plugins: [
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-
-    // Resolve source maps to the original source
     sourceMaps(),
   ],
 }
