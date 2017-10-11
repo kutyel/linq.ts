89f8d8cfb35754b58c90a02ca87a4e2c967dc857
kutyel <flaviocorpa@gmail.com>
2017-10-11 11:28:21 +0200

    chore(browser): Replace Webpack with Rollup

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,24 +1,15 @@
-import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 
-const pkg = require('./package.json')
-
-const moduleName = 'linq'
-
 export default {
-  entry: `dist/${moduleName}.js`,
-  targets: [
-    { dest: pkg['umd:main'], moduleName, format: 'umd' },
+  input: 'dist/linq.js',
+  output: [
+    { file: 'dist/linq.umd.js', name: 'linq', format: 'umd', exports: 'named' },
   ],
-  sourcemap: true,
   external: [],
   watch: {
     include: 'dist/**',
   },
-  plugins: [
-    commonjs(),
-    resolve(),
-    sourceMaps(),
-  ],
+  plugins: [commonjs(), resolve(), sourceMaps()],
 }
--- test/enum.ts
+++ test/enum.ts
@@ -2,15 +2,18 @@ import test from 'ava'
 import { Enumerable } from '../linq'
 
 test('Range', t => {
-  t.is(
-    Enumerable.Range(1, 10).Select(x => x * x).ToArray().toString(),
-    '1,4,9,16,25,36,49,64,81,100'
+  t.deepEqual(
+    Enumerable.Range(1, 10)
+      .Select(x => x * x)
+      .ToArray(),
+    [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
   )
 })
 
 test('Repeat', t => {
-  const test = Enumerable.Repeat('I like programming.', 3)
-  t.is(test.ElementAt(0), 'I like programming.')
-  t.is(test.ElementAt(1), 'I like programming.')
-  t.is(test.ElementAt(2), 'I like programming.')
+  const str = 'I like programming'
+  const test = Enumerable.Repeat(str, 3)
+  t.is(test.ElementAt(0), str)
+  t.is(test.ElementAt(1), str)
+  t.is(test.ElementAt(2), str)
 })
