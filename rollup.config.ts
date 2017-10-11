de9ef3bd56dce2f80b97af85c1c9b392af95f4fb
Asier Ferro <asierferro@gmail.com>
2017-10-11 11:11:31 +0200

    Replace Webpack with Rollup (#88)
    * Replace Webpack with Rollup
    
    * Deleted webpack files and useless modules
    
    * Deleted webpack config file and change variable rollup config file
    
    * Removed comments on rollup config files
    
    * Delete duplicated variable

---
+++ rollup.config.ts
@@ -0,0 +1,24 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+
+const pkg = require('./package.json')
+
+const moduleName = 'linq'
+
+export default {
+  entry: `dist/${moduleName}.js`,
+  targets: [
+    { dest: pkg['umd:main'], moduleName, format: 'umd' },
+  ],
+  sourcemap: true,
+  external: [],
+  watch: {
+    include: 'dist/**',
+  },
+  plugins: [
+    commonjs(),
+    resolve(),
+    sourceMaps(),
+  ],
+}
